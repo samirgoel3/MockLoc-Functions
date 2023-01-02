a0ff8b80443722007f2d0799559f2a885e3eebed
Samir Goel <samirgoel3@gmail.com>
2023-01-02 19:00:44 +0530

    Update sync-stationary-points.js

--- functions/sync-stationary-points.js
+++ functions/sync-stationary-points.js
@@ -1,18 +1,27 @@
 const axios = require('axios');
 const { getHeader } = require('../api-helper');
+const { failedResponse, successResponse } = require('../api-helper/response-handler');
 
 exports.handler = async (event, context) => {
+    try{
+        const body = JSON.parse(event.body)
+        console.log("***** "+body);
+        return successResponse("Response is fetched successfully", body)
 
-    console.log("***** This is for testing only ")
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                result: 1,
-                messsage: "Result fetch successfully",
-                response: "This is for synchronising stationary points"
-            }
-        )
+    }catch(e){
+        return failedResponse("Exception in sync-stationary-points " + e.message)
     }
+
+    // console.log("***** This is for testing only ")
+
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify(
+    //         {
+    //             result: 1,
+    //             messsage: "Result fetch successfully",
+    //             response: "This is for synchronising stationary points"
+    //         }
+    //     )
+    // }
 }
