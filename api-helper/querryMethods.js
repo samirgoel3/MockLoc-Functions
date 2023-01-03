75f1cba39337eb7de81a9bedc2c3efed5bce2cd5
Samir Goel <samirgoel3@gmail.com>
2023-01-03 14:38:06 +0530

    Completed Stationary Point Sync Api Calculation part

--- api-helper/querryMethods.js
+++ api-helper/querryMethods.js
@@ -2,3 +2,5 @@ exports.FIND_ONE = "https://data.mongodb-api.com/app/data-fiicp/endpoint/data/v1
 exports.FIND_ALL = "https://data.mongodb-api.com/app/data-fiicp/endpoint/data/v1/action/find"
 exports.UPDATE_ONE = "https://data.mongodb-api.com/app/data-fiicp/endpoint/data/v1/action/updateOne"
 exports.INSERT_ONE = "https://data.mongodb-api.com/app/data-fiicp/endpoint/data/v1/action/insertOne"
+exports.INSERT_MANY = "https://data.mongodb-api.com/app/data-fiicp/endpoint/data/v1/action/insertMany"
+exports.DELETE_MANY = "https://data.mongodb-api.com/app/data-fiicp/endpoint/data/v1/action/deleteMany"
--- functions/sync-stationary-points.js
+++ functions/sync-stationary-points.js
@@ -1,57 +1,182 @@
 const axios = require('axios');
 const { getHeader } = require('../api-helper');
-const { FIND_ALL } = require('../api-helper/querryMethods');
+const { FIND_ALL, DELETE_MANY, INSERT_MANY } = require('../api-helper/querryMethods');
 const { failedResponse, successResponse } = require('../api-helper/response-handler');
 
 exports.handler = async (event, context) => {
-    try{
+    try {
         const body = JSON.parse(event.body)
-        const incoming_points = body.stationary_points ;
+        const incoming_points = body.stationary_points;
 
-        console.log("***** INCOMING POINTS: "+incoming_points);
-        console.log("***** USER ID: "+body.user_id);
-
+        console.log("-----> Total Incoming point by user ID: " + body.user_id + ": " + incoming_points.length);
 
 
-        const querryToGetAllExistingPoints = {"collection": "stationarypoints","database": "mocklocations","dataSource": "mocklocations","filter":{"user_id":""+body.user_id}}
 
-        const allPointsOfUser = await axios.post(FIND_ALL, querryToGetAllExistingPoints, { headers: getHeader() });
-        if(allPointsOfUser.data.documents){console.log("***** This user does have some documents")}
-        else{console.log("***** This user does not have any documents")}
+        const querryToGetAllExistingPoints = { "collection": "stationarypoints", "database": "mocklocations", "dataSource": "mocklocations", "filter": { "user_id": "" + body.user_id } }
+
+        let axiosFindAllResponse = await axios.post(FIND_ALL, querryToGetAllExistingPoints, { headers: getHeader() });
+
+        let allExistingPointsOfUser = axiosFindAllResponse.data.documents;
+        if (allExistingPointsOfUser.length != 0) { console.log("-----> This user does already have some saved documents", " length=" + allExistingPointsOfUser.length) }
+        else { console.log("-----> This user does not have any saved documents") }
 
         /**
          * Check weather element to be update or create
          */
-        var elementsToUpdate = [] , elementsToCreate = [];
-        for(var i=0 ; i < incoming_points.length ; i++ ){
-            if( allPointsOfUser.data.documents.some(el => el.latitude == incoming_points[i].Latitude)){
+        var elementsToUpdate = [], elementsToCreate = [];
+        for (var i = 0; i < incoming_points.length; i++) {
+            if (allExistingPointsOfUser.some(el => el.latitude == incoming_points[i].Latitude)) {
                 elementsToUpdate.push(incoming_points[i])
-            }else{
+            } else {
                 elementsToCreate.push(incoming_points[i])
             }
         }
+        console.log("-----> Elements to update", elementsToUpdate.length)
+        console.log("-----> Elements to Insert", elementsToCreate.length)
 
-        var elementsToUpdate = [] , elementsToCreate = [];
-        console.log("***** Elements to update", elementsToUpdate.length())
-        console.log("***** Elements to Insert", elementsToCreate.length())
 
-        return successResponse("Response is fetched successfully", body)
+        if (elementsToUpdate.length > 0) {
+            let stationaryPointsToUpdate = getStationaryPointArrayToUpdateInDb(elementsToUpdate, body.user_id);
+            let stationaryPointToCreate = getStationaryPointArrayToAddInDb(elementsToCreate, allExistingPointsOfUser, body.user_id);
 
-    }catch(e){
-        console.log("******* Exception "+e.message);
+            // deleting existing docs
+            const querryDeleteExistingPoints = { "collection": "stationarypoints", "database": "mocklocations", "dataSource": "mocklocations", "filter": { "user_id": "" + body.user_id } }
+            const deletedDocument = await axios.post(DELETE_MANY, querryDeleteExistingPoints, { headers: getHeader() })
+            console.log("-----> Existing Document Deleted: ", JSON.stringify(deletedDocument.data))
+
+            // Collecting all existing and new point in single array 
+            const overallPointsToAddInDb = getOverallPoints(stationaryPointToCreate, stationaryPointsToUpdate, allExistingPointsOfUser);
+            console.log("-----> Overall ST Point need to add and update in DB: ", overallPointsToAddInDb.length);
+
+            // Inserting overall elements in DB 
+            const querryToInsertMultipleDocs = {
+                "collection": "stationarypoints",
+                "database": "mocklocations",
+                "dataSource": "mocklocations",
+                "documents": overallPointsToAddInDb
+            }
+            const overallInsertionResult = await axios.post(INSERT_MANY, querryToInsertMultipleDocs, { headers: getHeader() })
+            console.log("-----> Overall documents inserted response ", JSON.stringify(overallInsertionResult.data))
+            return successResponse("Overall documents inserted response", overallInsertionResult.data);
+
+        } else {
+            if (elementsToCreate.length > 0) {
+                let stationaryPointToCreate = getStationaryPointArrayToAddInDb(elementsToCreate, allExistingPointsOfUser, body.user_id);
+                console.log("Overall ST Point need to add in DB: ", stationaryPointToCreate.length);
+
+                // Inserting overall elements in DB 
+                const querryToInsertMultipleDocs = {
+                    "collection": "stationarypoints",
+                    "database": "mocklocations",
+                    "dataSource": "mocklocations",
+                    "documents": stationaryPointToCreate
+                }
+                const axiosResponse = await axios.post(INSERT_MANY, querryToInsertMultipleDocs, { headers: getHeader() })
+                return successResponse("Stationary Points added successfully ", axiosResponse.data)
+
+            }
+        }
+    } catch (e) {
+        console.log("******* Exception " + e.message);
         return failedResponse("Exception in sync-stationary-points " + e.message)
     }
 
-    // console.log("***** This is for testing only ")
+}
 
-    // return {
-    //     statusCode: 200,
-    //     body: JSON.stringify(
-    //         {
-    //             result: 1,
-    //             messsage: "Result fetch successfully",
-    //             response: "This is for synchronising stationary points"
-    //         }
-    //     )
-    // }
+
+/**
+ * 
+ * @param {array of incoming object from mobile} elementsToCreate 
+ * @param { array of existing point from DB} allExistingPointsOfUser 
+ * @param {string} userId 
+ * @returns this return a modulated custom object that matches object saved in DB
+ */
+const getStationaryPointArrayToAddInDb = (elementsToCreate, allExistingPointsOfUser, userId) => {
+    let stationaryPointToCreate = [];
+    let counter = allExistingPointsOfUser.length;
+    for (var i = 0; i < elementsToCreate.length; i++) {
+        stationaryPointToCreate.push({
+            user_id: "" + userId,
+            point_id: counter + 1,
+            name: elementsToCreate[i].Name,
+            description: elementsToCreate[i].Description,
+            latitude: elementsToCreate[i].Latitude,
+            longitude: elementsToCreate[i].Longitude,
+            geographic_address: elementsToCreate[i].geogrphicAddress,
+            icon: elementsToCreate[i].Icon,
+            timeZone: elementsToCreate[i].TimeZone,
+            is_favourite: elementsToCreate[i].IsFavourite,
+            hours_playback: elementsToCreate[i].HourPlayback,
+            playlist_linkage: elementsToCreate[i].PlayListLinkage,
+            selected_via: elementsToCreate[i].SelectedVia,
+            date: elementsToCreate[i].TimeStamp,
+            launches: elementsToCreate[i].launches,
+            sync_state: true
+        })
+        counter = counter + 1;
+    }
+    return stationaryPointToCreate;
+}
+
+
+/**
+ * 
+ * @param {array of incoming objects from mobile} elementsToUpdate 
+ * @param {string} userId 
+ * @returns this return a modulated custom object that will update  saved in DB
+ */
+const getStationaryPointArrayToUpdateInDb = (elementsToUpdate, userId) => {
+    let stationaryPointToUpdate = []
+    for (var i = 0; i < elementsToUpdate.length; i++) {
+        stationaryPointToUpdate.push(
+            {
+                user_id: userId,
+                point_id: "" + elementsToUpdate[i].id,
+                name: elementsToUpdate[i].Name,
+                description: elementsToUpdate[i].Description,
+                latitude: elementsToUpdate[i].Latitude,
+                longitude: elementsToUpdate[i].Longitude,
+                geographic_address: elementsToUpdate[i].geogrphicAddress,
+                icon: elementsToUpdate[i].Icon,
+                timeZone: elementsToUpdate[i].TimeZone,
+                is_favourite: elementsToUpdate[i].IsFavourite,
+                hours_playback: elementsToUpdate[i].HourPlayback,
+                playlist_linkage: elementsToUpdate[i].PlayListLinkage,
+                selected_via: elementsToUpdate[i].SelectedVia,
+                date: elementsToUpdate[i].TimeStamp,
+                launches: elementsToUpdate[i].launches,
+                sync_state: true
+            }
+        )
+    }
+    return stationaryPointToUpdate;
+
+}
+
+
+/**
+ * This method only used in case when there are documents to update so here we are summarising all doc in songle variable
+ * @param {array of stationary point object that is new in nature} elementsToCreate 
+ * @param {array of stationary point object that was already exist in DB} elementsToUpdate 
+ * @param {array of stationary point object that was already exist in DB but was removed} allExistingPointsOfUser 
+ */
+const getOverallPoints = (elementsToCreate, elementsToUpdate, allExistingPointsOfUser) => {
+    console.log("-----> Final calculation using -->  elementsToCreate:" + elementsToCreate.length + " elementsToUpdate:" + elementsToUpdate.length + " allExistingPointsOfUser:" + allExistingPointsOfUser.length)
+    const overallStationaryPoints = [];
+    for (var i = 0; i < elementsToCreate.length; i++) {
+        overallStationaryPoints.push(elementsToCreate[i])
+    }
+    for (var i = 0; i < elementsToUpdate.length; i++) {
+        overallStationaryPoints.push(elementsToUpdate[i])
+    }
+    for (var i = 0; i < allExistingPointsOfUser.length; i++) {
+        if (elementsToUpdate.some(el => el.latitude == allExistingPointsOfUser[i].latitude)) {
+
+        } else {
+            delete elementsToUpdate[i]._id
+            overallStationaryPoints.push(elementsToUpdate[i])
+        }
+    }
+
+    return overallStationaryPoints;
 }
