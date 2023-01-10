2a4df452910eee0a6e09a8261b0f167c6759c94d
Samir Goel <samirgoel3@gmail.com>
2023-01-11 01:17:47 +0530

    New API for getting all Itineraries

---
+++ functions/get-all-itineraries.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const { getHeader } = require('../api-helper');
+const { FIND_ONE } = require('../api-helper/querryMethods');
+const { failedResponse, successResponse } = require('../api-helper/response-handler');
+
+exports.handler = async (event, context) => {
+    try {
+        const body = JSON.parse(event.body)
+        if (!body.user_id) {
+            return failedResponse("please provide user_id")
+        } else {
+            console.log("-----> USER ID:  ", body.user_id)
+            return getAllItineraries(body.user_id)
+        }
+    } catch (e) {
+        return failedResponse("EXCEPTION in get-all-itineraries API --> " + e.message)
+    }
+}
+
+
+const getAllItineraries = async (userId) => {
+    try {
+        const QUERRY = {
+            "collection": "itineraries",
+            "database": "mocklocations",
+            "dataSource": "mocklocations",
+            "filter": { "user_id": userId, }
+        }
+
+        let res = await axios.post(FIND_ONE, QUERRY, { headers: getHeader() })
+        return successResponse("****  ", res.data)
+        // if (!res.data.documents) {
+        //     return failedResponse("No Itinerary found")
+        // } else {
+        //     return successResponse("You have " + res.data.documents.length + " playlist stored on server.", res.data.documents);
+        // }
+    } catch (e) {
+        return failedResponse("EXCEPTION in getAllItineraries " + e.message)
+    }
+}
