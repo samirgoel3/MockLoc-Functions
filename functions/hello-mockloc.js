2584e927c50629e6a0cc8209fc40e307f8d0ef72
samirgoel3 <samirgoel3@gmail.com>
2025-07-15 09:27:01 +0000

    changed the hello mock lock api message

--- functions/hello-mockloc.js
+++ functions/hello-mockloc.js
@@ -2,6 +2,6 @@ exports.handler = async (event, context) => {
 
     return {
         statusCode: 200,
-        body: "Hello Mockloc"
+        body: "Hello Mockloc now i will start work on you, please be my inspiration for what i have started i will finish that"
     }
 }
