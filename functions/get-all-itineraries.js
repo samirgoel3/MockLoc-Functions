49b0a0fdb916970d41839b2668170e84159dbeff
Samir Goel <samirgoel3@gmail.com>
2025-10-13 18:25:47 +0530

    Update get-all-itineraries.js

--- functions/get-all-itineraries.js
+++ functions/get-all-itineraries.js
@@ -1,7 +1,14 @@
-const axios = require('axios');
-const { getHeader } = require('../api-helper');
-const { FIND_ONE } = require('../api-helper/querryMethods');
 const { failedResponse, successResponse } = require('../api-helper/response-handler');
+const { MongoClient } = require('mongodb');
+
+let cachedClient = null;
+const getDb = async () => {
+    if (!cachedClient) {
+        cachedClient = new MongoClient(process.env.MONGO_DB_CONNECTION);
+        await cachedClient.connect();
+    }
+    return cachedClient.db('mocklocations');
+}
 
 exports.handler = async (event, context) => {
     try {
@@ -20,18 +27,13 @@ exports.handler = async (event, context) => {
 
 const getAllItineraries = async (userId) => {
     try {
-        const QUERRY = {
-            "collection": "itineraries",
-            "database": "mocklocations",
-            "dataSource": "mocklocations",
-            "filter": { "user_id": userId, }
-        }
-
-        let res = await axios.post(FIND_ONE, QUERRY, { headers: getHeader() })
-        if (!res.data.document) {
+        const db = await getDb();
+        const itinerariesCollection = db.collection('itineraries');
+        const doc = await itinerariesCollection.findOne({ user_id: userId });
+        if (!doc) {
             return failedResponse("No Itinerary found")
         } else {
-            return successResponse("itineraries found successfully.", res.data.document);
+            return successResponse("itineraries found successfully.", doc);
         }
     } catch (e) {
         return failedResponse("EXCEPTION in getAllItineraries " + e.message)
