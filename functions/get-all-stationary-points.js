d4a71777c53401bd32b16d083a68cdc1250b8f37
Samir Goel <samirgoel3@gmail.com>
2025-10-13 18:29:15 +0530

    Update get-all-stationary-points.js

--- functions/get-all-stationary-points.js
+++ functions/get-all-stationary-points.js
@@ -1,7 +1,14 @@
-const axios = require('axios');
-const { getHeader } = require('../api-helper');
-const { FIND_ALL } = require('../api-helper/querryMethods');
 const { failedResponse, successResponse } = require('../api-helper/response-handler');
+const { MongoClient } = require('mongodb');
+
+let cachedClient = null;
+const getDb = async () => {
+    if (!cachedClient) {
+        cachedClient = new MongoClient(process.env.MONGO_DB_CONNECTION);
+        await cachedClient.connect();
+    }
+    return cachedClient.db('mocklocations');
+}
 
 exports.handler = async (event, context) => {
     try {
@@ -20,18 +27,13 @@ exports.handler = async (event, context) => {
 
 const getAllStationaryPoints = async (userId) => {
     try {
-        const QUERRY = {
-            "collection": "stationarypoints",
-            "database": "mocklocations",
-            "dataSource": "mocklocations",
-            "filter": { "user_id": userId, }
-        }
-
-        let res = await axios.post(FIND_ALL, QUERRY, { headers: getHeader() })
-        if (res.data.documents.length == 0) {
+        const db = await getDb();
+        const stationaryCollection = db.collection('stationarypoints');
+        const docs = await stationaryCollection.find({ user_id: userId }).toArray();
+        if (!docs || docs.length === 0) {
             return failedResponse("No Stationary points found for this user")
         } else {
-            return successResponse("You have " + res.data.documents.length + " stationary points stored on server.", res.data.documents);
+            return successResponse("You have " + docs.length + " stationary points stored on server.", docs);
         }
     } catch (e) {
         return failedResponse("EXCEPTION in getVideoTutorials " + e.message)
