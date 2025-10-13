9fe79d958b0bfef2e7dd2cf39074fdf86d28a3f7
Samir Goel <samirgoel3@gmail.com>
2025-10-13 12:57:57 +0530

    changes in test file

--- functions/tester.js
+++ functions/tester.js
@@ -1,32 +1,22 @@
-const axios = require('axios');
-const { getHeader } = require('../api-helper');
+import { MongoClient } from "mongodb";
 
-exports.handler = async (event, context) => {
+let cachedClient = null;
 
-    console.log(event.body)
-
-    
-    const data = {
-        "collection": "users",
-        "database": "mocklocations",
-        "dataSource": "mocklocations"
+export async function handler(event, context) {
+  try {
+    if (!cachedClient) {
+      cachedClient = new MongoClient("mongodb+srv://samir:11223344@mocklocations.zymnm.mongodb.net/");
+      await cachedClient.connect();
     }
 
-    const res = await axios.post("https://data.mongodb-api.com/app/data-fiicp/endpoint/data/v1/action/find", data, {
-        headers: getHeader()
-    });
-
-    // const res = await axios.get("https://jsonplaceholder.typicode.com/users/1");
-
+    const db = cachedClient.db("mydb");
+    const users = await db.collection("users").find().toArray();
 
     return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                result: 1,
-                messsage: "Result fetch successfully",
-                response: res.data
-            }
-        )
-    }
+      statusCode: 200,
+      body: JSON.stringify(users),
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.message };
+  }
 }
